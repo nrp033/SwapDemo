593fd43a323981f606268f6591af8d3fd51c8aa5
Nitin Pawar <107263213+nrp033@users.noreply.github.com>
2022-10-31 14:07:55 +0530

    Swap two Numbers For Workshop Demo

---
+++ SwapDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace SwapDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Swapping the Number ");
+            Console.Write("\nEnter value of a : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter Value of b : ");
+            int b= Convert.ToInt32(Console.ReadLine()); 
+
+            int c=a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("After Swapping a : "+a);
+            Console.WriteLine("After Swapping b : " +b);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
